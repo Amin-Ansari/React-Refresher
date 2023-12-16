b08a94b3efd3abdbdb8ab474ab087a7cbdcf53d8
Amin Ansari <devansari@yahoo.com>
2023-12-16 12:59:51 +0330

    Adding task is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+
+import "./App.css";
+
 function App() {
-  return <div className="app"></div>;
+  const [tasksList, updateTheTasksList] = useState([]);
+  const [taskTitle, setTheTaskTitle] = useState("");
+
+  const taskTitleValueHandler = (eventObj) => {
+    setTheTaskTitle(eventObj.target.value);
+  };
+
+  const taskAddHandler = () => {
+    const task = {
+      title: taskTitle,
+      status: "do",
+    };
+    updateTheTasksList((state) => [...state, task]);
+  };
+
+  return (
+    <div className="app">
+      <form method="post">
+        <label htmlFor="#title">Task title</label>
+        <input
+          type={"text"}
+          onChange={taskTitleValueHandler}
+          id="title"
+        ></input>
+        <button type="button" onClick={taskAddHandler}>
+          Add the Task
+        </button>
+      </form>
+      <ul className="todo-list">
+        {tasksList.map((item, index) => (
+          <li className={item.status} key={index}>
+            {item.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
