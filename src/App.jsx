40fc2f6b91e483c2980ccab13e4e0000bc4e67c8
Amin Ansari <devansari@yahoo.com>
2023-12-16 21:36:17 +0330

    The controlled-form is don

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+
+import Form from "./Components/Form";
+import "./App.css";
+
 function App() {
-  return <div className="app"></div>;
+  const [userName, setTheUserName] = useState("");
+  const [password, setThePassword] = useState("");
+
+  const userInfo = {};
+
+  function resetThepassAndUserName() {
+    setTheUserName("");
+    setThePassword("");
+  }
+
+  const passwordValueHandler = (value) => {
+    setThePassword(value);
+  };
+  const userNameValueHandler = (value) => {
+    setTheUserName(value);
+  };
+
+  const onFormSubmission = (e) => {
+    e.preventDefault();
+    userInfo["userNameValue"] = userName;
+    userInfo.passwordValue = password;
+
+    resetThepassAndUserName();
+    console.log(userInfo);
+  };
+
+  return (
+    <div className="app">
+      <Form
+        userNameValue={userName}
+        passwordValue={password}
+        userNameFunction={userNameValueHandler}
+        passwordFunction={passwordValueHandler}
+        formSubmitHandler={onFormSubmission}
+      />
+    </div>
+  );
 }
 
 export default App;
