242698fa76b379e07610022d5fd5bd61cefd892a
Amin Ansari <devansari@yahoo.com>
2023-12-17 19:45:21 +0330

    The windowsResizer is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div className="app"></div>;
+  const [windowDemintions, updateWindowDemintions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    function resizeHandler() {
+      updateWindowDemintions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    window.addEventListener("resize", resizeHandler);
+
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, []);
+
+  return (
+    <div className="app">
+      <p className="window-width">{windowDemintions.width}px</p>
+      <p className="window-height">{windowDemintions.height}px</p>
+    </div>
+  );
 }
 
 export default App;
