80b35af37c2ae37178dd2c357a814313d332d906
Amin Ansari <devansari@yahoo.com>
2023-12-17 20:12:09 +0330

    The complex-counter is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,49 @@
+import { useReducer } from "react";
+import "./App.css";
+
+const reducerFunction = (state, action) => {
+  const newValue = { ...state };
+  if (action.type === "NORMAL-INCREASE" && newValue.num < 99) {
+    newValue.num = ++newValue.num;
+  } else if (action.type === "NORMAL-DECREASE" && newValue.num >= 1) {
+    newValue.num = --newValue.num;
+  } else if (action.type === "DYNAMIC-INCREASE" && newValue.num < 99) {
+    newValue.num = newValue.num + action.dynamicNumber;
+  } else if (action.type === "DYNAMIC-DECREASE" && newValue.num >= 1) {
+    newValue.num = newValue.num - action.dynamicNumber;
+  }
+  return newValue;
+};
+
 function App() {
-  return <div className="app"></div>;
+  const [counterReducer, dispatchReducer] = useReducer(reducerFunction, {
+    num: 0,
+  });
+
+  const normalIncrement = () => {
+    dispatchReducer({ type: "NORMAL-INCREASE" });
+  };
+  const normalDecrement = () => {
+    dispatchReducer({ type: "NORMAL-DECREASE" });
+  };
+  const dynamicIncrement = () => {
+    dispatchReducer({ type: "DYNAMIC-INCREASE", dynamicNumber: 4 });
+  };
+  const dynamicDecrement = () => {
+    dispatchReducer({ type: "DYNAMIC-DECREASE", dynamicNumber: 3 });
+  };
+
+  return (
+    <div className="app">
+      <p>{counterReducer.num}</p>
+      <div>
+        <button onClick={normalIncrement}>Increase by 1</button>
+        <button onClick={normalDecrement}>Decrease by 1</button>
+        <button onClick={dynamicIncrement}>Increase by dynamicNumber</button>
+        <button onClick={dynamicDecrement}>Decrease by dynamicNumber</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
